1945703bc0fa0f203a846d51e5d17343adb11ffa
MoizKhan5465 <moizsardar056@gmail.com>
2026-04-18 11:16:22 +0500

    feat(api): add authenticated current-user category listing endpoint

--- core/expensetracker/urls.py
+++ core/expensetracker/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path,include
 from expensetracker.views import home
+from expensetracker.views import CategoryListView
 
 urlpatterns = [
 
     path('', view=home, name='home'),
+    path('categories/', view=CategoryListView.as_view(), name='category-list'),
 ]
--- core/expensetracker/views.py
+++ core/expensetracker/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from rest_framework import generics, permissions
+from .models import Category
+from .serilizer import CategorySerializer   
 
 # Create your views here.
+
+
+class CategoryListView(generics.ListCreateAPIView):
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+       
 
 
 def home(request):
